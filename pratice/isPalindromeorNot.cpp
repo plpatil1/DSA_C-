e6317e5645fcc9978dd3d0e48039d2509bd5b6b9
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-13 00:29:19 +0530

    Update isPalindromeorNot.cpp

--- pratice/isPalindromeorNot.cpp
+++ pratice/isPalindromeorNot.cpp
@@ -1,37 +1,26 @@
-#include<iostram>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-  cout<<"enter the Name";
-  string name;
-  cin>>name;
-   int cnt=0;
-  int n=name.length();
-  int m=n/2;
-  for(int i=0, int j=m; i<m; i++, j++)
-    {
-        if(n%2==0)
-        {
-          if(name[i]!=name[j])
-          {
-            cnt++;
+int main() {
+    cout << "Enter the Name: ";
+    string name;
+    cin >> name;
+
+    int n = name.length();
+    bool isPalindrome = true;
+
+    for (int i = 0; i < n / 2; i++) {
+        if (name[i] != name[n - 1 - i]) {
+            isPalindrome = false;
             break;
         }
-    } else 
-        {
-            if(name[i]!=name[j+1])
-            {
-               cnt++;
-              break;
-            }
-        }
-}
-  if(cnt==0)
-  {
-    cout<<"it is Palindrome String";
-  } else {
-    cout<<"Not Palindrome String ";
-  }
+    }
 
+    if (isPalindrome) {
+        cout << "It is a Palindrome String";
+    } else {
+        cout << "Not a Palindrome String";
+    }
+
+    return 0;
 }
